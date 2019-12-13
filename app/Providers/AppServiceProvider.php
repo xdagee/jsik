335151db39de4b2f74f83fbe4ab533778b4f4212
princegee <14103448+xdagee@users.noreply.github.com>
2019-12-13 22:23:52 +0000

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Schema\Builder;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,6 +15,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        Builder::defaultStringLength(125);
     }
 
     /**
